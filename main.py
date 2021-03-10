3f980fb8d9962ae0b6a03d6393d0962db784170d
Flusza <Zachary.Howard@outlook.com>
2021-03-10 13:25:28 +0000

    Restructure Webserver

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import asyncio
 from aiohttp import web
-import cv2
+from cv2 import VideoCapture, flip, resize, imencode
 
 from mixins import LoggingMixin, ConfigMixin
 
 
 class WebServer(ConfigMixin, LoggingMixin):
-    def __init__(self, camera_stream_url: str) -> None:
+    def __init__(self) -> None:
         self.app = web.Application()
         self.app.router.add_get("/", self.handle_index)
         self.runner = None
 
-        self._stream = cv2.VideoCapture(camera_stream_url)
-        self._video_stream_url = self.config.VIDEO_STREAM_URL
+        self._stream = VideoCapture(self.config.VIDEO_STREAM_URL)
+        if not self._stream.isOpened():
+            raise RuntimeError('Cannot open camera')
 
     async def start(self) -> None:
         """
@@ -23,7 +24,7 @@ class WebServer(ConfigMixin, LoggingMixin):
         site = web.TCPSite(self.runner, 'localhost', self.config.WEB_SERVER_PORT)
         await site.start()
 
-    async def handle_index(self, request):
+    async def handle_index(self, request) -> web.StreamResponse:
         """
         Called when "/" is called.
         """
@@ -31,23 +32,19 @@ class WebServer(ConfigMixin, LoggingMixin):
         response.content_type = 'multipart/x-mixed-replace; boundary=frame'
         await response.prepare(request)
 
-        for frame in self.frames(self._video_stream_url):
+        for frame in self.frames():
             await response.write(frame)
         return response
 
-    def frames(self, path):
-        camera = cv2.VideoCapture(path)
-        if not camera.isOpened():
-            raise RuntimeError('Cannot open camera')
-
+    def frames(self):
         while True:
-            _, img = camera.read()
-            img = cv2.flip(cv2.resize(img, (1920, 1080)), 0)
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            _, img = self._stream.read()
+            img = flip(resize(img, (1920, 1080)), 0)
+            frame = imencode('.jpg', img)[1].tobytes()
             yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
 
 
-server = WebServer("http://192.168.0.93:12345")
+server = WebServer()
 loop = asyncio.get_event_loop()
 loop.create_task(server.start())
 loop.run_forever()
